8439bec71424c10d5f616af4d4ce1ead0268d994
Hans Hoffman <hans@flatfile.io>
2023-06-15 08:24:28 -0600

    make tests cleaner

--- test/Main.purs
+++ test/Main.purs
@@ -17,98 +17,23 @@ main = launchAff_ $ runSpec [consoleReporter] do
     let
       errMsg = "Expected one of [ 't', 'f', 'y', 'n', '1', '0', 'on', 'off', 'yes', 'no', 'true', 'false' ]"
       dummyMsg = "foobar"
-    it "should handle 't' shorthand" do
-      let
-        actual = parse_ "t"
-        expected = true
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'y' shorthand" do
-      let
-        actual = parse_ "y"
-        expected = true
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle '1' shorthand" do
-      let
-        actual = parse_ "1"
-        expected = true
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'f' shorthand" do
-      let
-        actual = parse_ "f"
-        expected = false
-      (fromRight true actual) `shouldEqual` expected
-    it "should handle 'n' shorthand" do
-      let
-        actual = parse_ "n"
-        expected = false
-      (fromRight true actual) `shouldEqual` expected
-    it "should handle '0' shorthand" do
-      let
-        actual = parse_ "0"
-        expected = false
-      (fromRight true actual) `shouldEqual` expected
-    it "should handle 'on'" do
-      let
-        actual = parse_ "on"
-        expected = true
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'true'" do
-      let
-        actual = parse_ "true"
-        expected = true
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'yes'" do
-      let
-        actual = parse_ "yes"
-        expected = true
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'off'" do
-      let
-        actual = parse_ "off"
-        expected = false
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'false'" do
-      let
-        actual = parse_ "false"
-        expected = false
-      (fromRight false actual) `shouldEqual` expected
-    it "should handle 'no'" do
-      let
-        actual = parse_ "no"
-        expected = false
-      (fromRight false actual) `shouldEqual` expected
-    it "should fail on 't' shorthand" do
-      let
-        actual = parse_ "tx"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
-    it "should fail on 'y' shorthand" do
-      let
-        actual = parse_ "yx"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
-    it "should fail on '1' shorthand" do
-      let
-        actual = parse_ "10"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
-    it "should fail on unknown shorthand" do
-      let
-        actual = parse_ "x"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
-    it "should fail on 'f' shorthand" do
-      let
-        actual = parse_ "foo"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
-    it "should fail on 'n' shorthand" do
-      let
-        actual = parse_ "nx"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
-    it "should fail on '0' shorthand" do
-      let
-        actual = parse_ "01"
-        expected = errMsg
-      (fromLeft dummyMsg actual) `shouldEqual` expected
+    it "should handle all valid shorthand values" do
+      (fromRight false $ parse_ "t") `shouldEqual` true
+      (fromRight false $ parse_ "y") `shouldEqual` true
+      (fromRight false $ parse_ "1") `shouldEqual` true
+      (fromRight true $ parse_ "f") `shouldEqual` false
+      (fromRight true $ parse_ "n") `shouldEqual` false
+      (fromRight true $ parse_ "0") `shouldEqual` false
+    it "should handle all valid longhand values" do
+      (fromRight false $ parse_ "true") `shouldEqual` true
+      (fromRight false $ parse_ "yes") `shouldEqual` true
+      (fromRight false $ parse_ "on") `shouldEqual` true
+      (fromRight true $ parse_ "false") `shouldEqual` false
+      (fromRight true $ parse_ "no") `shouldEqual` false
+      (fromRight true $ parse_ "off") `shouldEqual` false
+    it "should fail on 'close' values" do
+      (fromLeft dummyMsg $ parse_ "tr") `shouldEqual` errMsg
+      (fromLeft dummyMsg $ parse_ "ye") `shouldEqual` errMsg
+      (fromLeft dummyMsg $ parse_ "10") `shouldEqual` errMsg
+      (fromLeft dummyMsg $ parse_ "01") `shouldEqual` errMsg
+      (fromLeft dummyMsg $ parse_ "yess") `shouldEqual` errMsg
